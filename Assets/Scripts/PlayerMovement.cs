0969f497cbe00362c29afba59b248255a6e1b002
Juanita96 <juanaklois@gmail.com>
2025-09-10 17:20:13 -0300

    learn how to move character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController : MonoBehaviour
+{
+
+    [SerializeField] private InputActionReference moveAction;
+    private Vector2 moveInput;
+
+    [SerializeField] private InputActionReference jumpAction;
+    private bool jumpInput;
+
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+    bool isGrounded = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        moveAction.action.started += HandleMoveInput;
+        moveAction.action.performed += HandleMoveInput;
+        moveAction.action.canceled += HandleMoveInput;
+
+        jumpAction.action.performed += HandleJumpInput;
+    }
+
+    void HandleMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void HandleJumpInput(InputAction.CallbackContext context)
+    {
+        if (isGrounded == true)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+}
